db5bcf762a84a5db5f6544aa326d2f02dbee6d12
maxdignan <maxdignan@gmail.com>
2015-04-12 19:21:30 -0400

    distances controller ready

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     #TODO need to change to specific verbs
     resources :riders
     resources :rides
+    resources :distances
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
